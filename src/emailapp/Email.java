214bbb7e0fab5639ce51e70c724faf8780c187d6
aburk3 <aburk3@gmail.com>
2019-07-30 10:43:21 -0500

    Worked on some refactoring

--- src/emailapp/Email.java
+++ src/emailapp/Email.java
@@ -17,11 +17,9 @@ public class Email {
 	public Email(String firstName, String lastName) {
 		this.firstName = firstName;
 		this.lastName = lastName;
-		System.out.println("EMAIL CREATED: " + this.firstName + " " + this.lastName);
 		
 		// Call a method asking for the department - return the department
 		this.department = setDepartment();
-		System.out.println("Department: " + this.department);
 		
 		// Call a method that returns a random password
 		this.password = randomPassword(defaultPasswordLength);
@@ -29,12 +27,11 @@ public class Email {
 		
 		// Combine elements to generate email
 		email = firstName.toLowerCase() + "." + lastName.toLowerCase() + "@" + department + "." + companySuffix;
-		System.out.println("Your email is: " + email);
 	}
 	
 	// Ask for the department
 	private String setDepartment() {
-		System.out.println("DEPARTMENT CODES\n1 for Sales\n2 for Development\n3 for Accounting\n0 for none\n Enter department code: ");
+		System.out.println("New worker: " + firstName + ". Department Codes:\n1 for Sales\n2 for Development\n3 for Accounting\n0 for none\n Enter department code: ");
 		Scanner in = new Scanner(System.in);
 		int depChoice = in.nextInt();
 		if (depChoice == 1) { return "sales"; }
@@ -76,8 +73,8 @@ public class Email {
 	
 	public String showInfo() {
 		return "DISPLAY NAME: " + firstName + " " + lastName +
-				"COMPANY EMAIL: " + email +
-				"MAILBOX CAPACITY: " + mailboxCapacity + "mb";
+				"\nCOMPANY EMAIL: " + email +
+				"\nMAILBOX CAPACITY: " + mailboxCapacity + "mb";
 	}
 	
 	
